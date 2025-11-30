f28178917852d0e4fcd6356c16db172473fc04b3
Dkflora <dkflora13@gmail.com>
2025-11-29 17:44:00 -0700

    modifying codes

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket       = "makasiw7backet" # Replace with your bucket name
-    key          = "uat/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = false
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket       = "" # Replace with your bucket name
+#     key          = "uat/terraform.tfstate"
+#     region       = "us-east-1"
+#     encrypt      = true
+#     use_lockfile = false
+#   }
+# }
