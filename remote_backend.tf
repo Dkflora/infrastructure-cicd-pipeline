8f967e74c085cc080e5471d2180a9291d4c2ea52
Dkflora <dkflora13@gmail.com>
2025-11-29 17:10:59 -0700

    tkt0428 infra

---
+++ remote_backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "makasiw7backet" # Replace with your bucket name
+    key          = "uat/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = false
+  }
+}
